3bcb872ffecdbca30406bea5e08e5b5368fe4f5b
Andrew Rzepecki <anrzepec@student.42.fr>
2023-03-26 20:22:59 +0200

    Added evaluate board heuristics comments

--- src/negamax.rs
+++ src/negamax.rs
@@ -12,7 +12,7 @@ pub fn alpha_beta_negamax(board: &mut Vec<Vec<i32>>, player: i32, depth: i32, mu
     
     for x in 0..board.len() {
         for y in 0..board[x].len() {
-            if is_legal(board, x as i32, y as i32, player) {
+            if is_legal(board, x as i32, y as i32, player)  {
                 // Play the move
                 board[x][y] = player;
                 
@@ -21,7 +21,7 @@ pub fn alpha_beta_negamax(board: &mut Vec<Vec<i32>>, player: i32, depth: i32, mu
                 let score = -score;
                 
                 // Undo the move
-                board[x][y] = 0;
+                board[x][y] = UNPLAYED_STATE;
                 
                 // Check if this move is the best so far
                 if score > best_score {
@@ -30,10 +30,7 @@ pub fn alpha_beta_negamax(board: &mut Vec<Vec<i32>>, player: i32, depth: i32, mu
                 }
                 
                 // Apply alpha-beta pruning
-                if best_score > alpha {
-                    alpha = best_score;
-                }
-                
+                alpha = if best_score > alpha {best_score} else {alpha};
                 if alpha >= beta {
                     return (best_pos.unwrap().0, best_pos.unwrap().1, best_score);
                 }
@@ -44,5 +41,18 @@ pub fn alpha_beta_negamax(board: &mut Vec<Vec<i32>>, player: i32, depth: i32, mu
 }
 
 fn evaluate_board(board: &mut Vec<Vec<i32>>, player : i32) -> i32 {
+    // According to https://www.scirp.org/journal/paperinformation.aspx?paperid=90972
+    // Feature 1: Absolute win
+    //      Four chessmen are connected horizontally, vertically or diagonally as shown in Figure 5: (a, 1)-(b, 2)-(c, 3)-(d, 4).
+    // Feature 2: Three connected chessmen
+    //      Three chessmen are connected horizontally, vertically or diagonally as shown in Figure 5: (c, 2)-(d, 2)-(e, 2) or (c, 1)-(d, 2)-(f, 4).
+    // Feature 3: Two connected chessmen
+    //      Two chessmen are connected horizontally, vertically or diagonally as shown in Figure 5: (b, 1)-(c, 1).
+    // Feature 4: Single chessman
+    //      A chessman that is not connected to another same chessman horizontally, vertically or diagonally as shown in Figure 5: (d, 1).
     1
+}
+
+fn should_eval(board: &mut Vec<Vec<i32>>, player: i32) -> bool {
+    true
 }
